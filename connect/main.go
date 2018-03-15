353b9353138ee6aeb7f24a69c4e3919994d1529f
dkkahn10 <dustin@dustinkahn.com>
2018-03-15 15:35:05 -0400

    table successfully added via exec

--- connect/main.go
+++ connect/main.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/dkkahn10/go-pgx"
 )
 
 func main() {
-	conn := util.Connect("connect test")
+	conn := util.Connect("Creating table in Windows postgreSQL")
 	defer conn.Close()
+
+	_, err := conn.Exec(`
+		CREATE TABLE cards (
+			id integer NOT NULL,
+			board_id integer NOT NULL,
+			data jsonb
+		);
+	`)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to create users table: %v\n", err)
+		os.Exit(1)
+	}
 	fmt.Printf("Connection worked!\n")
 }
